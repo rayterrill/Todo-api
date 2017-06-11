79f7808997ce3de755755ed4f546f1be36f37f63
Ray Terrill <rayjohnterrill@gmail.com>
2017-06-11 14:45:02 -0700

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db =  require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -49,19 +50,25 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 	
-	body.description = body.description.trim();
+	//if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	//	return res.status(400).send();
+	//}
+	
+	//body.description = body.description.trim();
 	
 	// add id field
-	body.id = todoNextId++;
+	//body.id = todoNextId++;
 	
-	todos.push(body);
+	//todos.push(body);
 	
-	console.log('description: ' + body.description);
-	res.json(body);
+	//console.log('description: ' + body.description);
+	//res.json(body);
 });
 
 // DELETE /todos/:id
@@ -104,6 +111,8 @@ app.put('/todos/:id', function (req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function () {
-	console.log('Express listening on PORT ' + PORT + '!');
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function () {
+		console.log('Express listening on PORT ' + PORT + '!');
+	});
 });
