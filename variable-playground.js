51a502cf8b98f29895da782a6f27d3b10ea8e81f
Ray Terrill <rayjohnterrill@gmail.com>
2017-06-10 18:42:58 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+/*
+var person = {
+	name: 'Andrew',
+	age: 21
+};
+
+function updatePerson (obj) {
+	//obj = {
+	//	name: 'Andrew',
+	//	age: 24
+	//};
+	obj.age = 24;
+}
+
+updatePerson(person);
+console.log(person);
+*/
+
+var grades = [15, 88];
+
+function addGrades (grades) {
+	grades.push(55);
+	debugger;
+	
+	//grades [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
